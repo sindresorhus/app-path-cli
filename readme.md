87b73e4f87009aeabc4bcfee9a96d25275763477
Sindre Sorhus <sindresorhus@gmail.com>
2016-03-08 22:08:54 +0700

    Node.js >=4 now required

--- readme.md
+++ readme.md
@@ -33,4 +33,4 @@ $ app-path --help
 
 ## License
 
-MIT © [Sindre Sorhus](http://sindresorhus.com)
+MIT © [Sindre Sorhus](https://sindresorhus.com)
--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-'use strict';
-var path = require('path');
-var test = require('ava');
-var childProcess = require('child_process');
+import test from 'ava';
+import execa from 'execa';
 
-test(function (t) {
-	t.plan(2);
-
-	childProcess.execFile('./cli.js', ['Safari'], {cwd: __dirname}, function (err, stdout) {
-		t.assert(!err, err);
-		t.assert(path.basename(stdout.trim()) === 'Safari.app');
-	});
+test(async t => {
+	t.is((await execa('./cli.js', ['Safari'])).stdout, '/Applications/Safari.app');
 });
